c8683da5c14be281a607b790ab7c91d46ddec93a
Victor Truong <me@ifvictr.com>
2018-04-22 14:56:36 +0000

    Working state

--- server/server.js
+++ server/server.js
@@ -18,15 +18,16 @@ let invites = {
     //     to: "id"
     // }
 };
-let games = [
-    // [player id, player id]
+let games = {
     // "from+to": ['id', 'id']
-];
+};
 
 io.on("connection", socket => {
     const id = socket.id;
+    // Add new player to list
     players[id] = { name: "test", open: true };
     console.log(`player ${id} joined`);
+    // Informational request
     socket.on("get players", () => {
         socket.emit("got players", players);
     });
@@ -45,17 +46,53 @@ io.on("connection", socket => {
     });
     socket.on("game invite response", ({ fromId, accepted }) => {
         if(accepted) {
-            const channelId = `${fromId}+${id}`;
+            players[fromId].open = false;
+            players[id].open = false;
+            const gameId = `${fromId}+${id}`;
             // Remove from invites
-            delete invites[channelId];
+            delete invites[gameId];
+            // Add to current games
+            games[gameId] = [fromId, id];
             // Create new channel
-            socket.join(channelId);
-            io.sockets.sockets[fromId].join(channelId);
-            console.log(io.sockets.adapter.rooms[channelId]);
+            socket.join(gameId);
+            io.sockets.sockets[fromId].join(gameId);
+            io.in(gameId).emit("game started", {
+                gameId,
+                gameInfo: games[gameId],
+                starting: Math.random() > 0.5 ? 0 : 1 // Gives random player
+            }); // broadcast game info to competing players
+            console.log(io.sockets.adapter.rooms[gameId]);
         }
         console.log(accepted);
     });
+    // When player moves paddle
+    socket.on("move", data => {
+        let newData = { ...data };
+        newData.turn = newData.turn === 1 ? 0 : 1; // Alternate turn and return
+        // data: {
+        //     gameId
+        //     gameInfo
+        //     pos: { x, y }
+        //     acceleration: { x, y }
+        //     turn // socket.id of the next hitter
+        // }
+
+        io.in(data.gameId).emit("moved", newData);
+    });
+    // Remove player from list
     socket.on("disconnect", () => {
+        // Iterates through invites and removes invites sent to the current player (now non-existent)
+        Object.keys(invites).forEach(inviteId => {
+            if(inviteId.includes(id)) {
+                delete invites[inviteId];
+            }
+        });
+        // Same thing for games
+        Object.keys(games).forEach(gameId => {
+            if(gameId.includes(id)) {
+                delete games[gameId];
+            }
+        });
         delete players[id];
         console.log(`player ${id} left`);
     });
